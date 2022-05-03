fd6c0bbed7bc015d54ce46fb7a5cf762ed48e0e1
luisma0330 <luisma0330@gmail.com>
2022-05-03 14:32:46 -0500

    Creación de archivos de prueba y la clase User

---
+++ test/models/user.test.js
@@ -0,0 +1,14 @@
+const User = require('../../app/models/user')
+
+describe('Unit test for User class', () => {
+    test('Create an User object', () => {
+        const user = new User(1, "luisma0330", "Luis", "Bio", "dateCreated", "lastUpdated")
+
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("luisma0330")
+        expect(user.name).toBe("Luis")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdate).toBe("lastUpdated")
+    })
+})
