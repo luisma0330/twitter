226fb269d32c4ddf3e235687b35d821f03751af4
luisma0330 <luisma0330@gmail.com>
2022-05-03 20:44:25 -0500

    Añadiendo las pruebas del requerimiento 3: Cada objeto tipo user necesita los siguietes gettes: , , , .

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -11,4 +11,12 @@ describe('Unit test for User class', () => {
         expect(user.dateCreated).not.toBeUndefined()
         expect(user.lastUpdated).not.toBeUndefined()
     })
+
+    test('Add getters', () => {
+        const user = new User(1, "luisma0330", "Luis", "Bio")
+        expect(user.getUsername()).toBe("luisma0330")
+        expect(user.getBio()).toBe("Bio")
+        expect(user.getDateCreated()).not.toBeUndefined()
+        expect(user.getLastUpdated()).not.toBeUndefined()
+    })
 })
