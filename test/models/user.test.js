3c902fdd0ac7234aa941f64cfb1e232fbd4ff2b7
luisma0330 <luisma0330@gmail.com>
2022-05-03 19:54:43 -0500

    Modificando la prueba para que dateCreated y lastUpdated no sean Undefined

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -2,13 +2,13 @@ const User = require('../../app/models/user')
 
 describe('Unit test for User class', () => {
     test('Create an User object', () => {
-        const user = new User(1, "luisma0330", "Luis", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "luisma0330", "Luis", "Bio")
 
         expect(user.id).toBe(1)
         expect(user.username).toBe("luisma0330")
         expect(user.name).toBe("Luis")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 })
