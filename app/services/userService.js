942a7df4434ac46aaae22162db3078ffa3940f13
luisma0330 <luisma0330@gmail.com>
2022-05-04 15:17:21 -0500

    Añadiendo la funcionalidad del requerimiento 3 del Sprint 2

--- app/services/userService.js
+++ app/services/userService.js
@@ -8,6 +8,11 @@ class UserService{
     static getInfo(user){
         return Object.values(user)
     }
+
+    static updateUserUsername(user, newUsername){
+        user.setUsername = newUsername
+    }
+
 }
 
 module.exports = UserService
