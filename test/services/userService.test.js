bc6de3072339e812f914c285127233d0b542544a
luisma0330 <luisma0330@gmail.com>
2022-05-04 12:53:45 -0500

    Añadiendo la prueba del segundo requerimiento del sprint 2

--- test/services/userService.test.js
+++ test/services/userService.test.js
@@ -11,4 +11,13 @@ describe("Test for UserService", () => {
         expect(user.bio).not.toBeUndefined()
     })
 
+    test("2. Gell all user data in a list", () => {
+        const user = UserService.create(1, "luisma0330", "Luis")
+        const userInfoList = UserService.getInfo(user)
+        expect(userInfoList[0]).toBe(1)  
+        expect(userInfoList[1]).toBe("luisma0330")
+        expect(userInfoList[2]).toBe("Luis")
+        expect(userInfoList[3]).toBe("Sin bio")
+    })
+
 })
