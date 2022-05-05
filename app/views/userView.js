57fe3325efd507c919325d18e6cde7b48bb6a301
luisma0330 <luisma0330@gmail.com>
2022-05-05 01:30:42 -0500

    Añadiendo la funcionalidad del requerimiento 4 del sprint 3

--- app/views/userView.js
+++ app/views/userView.js
@@ -1,9 +1,12 @@
+const UserService = require('./../services/userService')
 const User = require('./../services/userService')
 
 class userView{
     static createUser(payload){
         if(payload === null){
             return {error: `payload no existe`}
+        }else if(typeof payload.id === "number" && typeof payload.username === "string" && typeof payload.name === "string"){
+            return UserService.create(payload.id, payload.username, payload.name)
         }else{
             return {error: 'necesitan tener un valor válido'}
         }
